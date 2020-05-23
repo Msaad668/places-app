be65529a2ef3858e9549c4ba4b0897524797bbce
unknown <mostafasaad55@gmail.com>
2020-05-23 10:35:12 +0200

    server initialized

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`serverstarted on port ${PORT}`);
+});
