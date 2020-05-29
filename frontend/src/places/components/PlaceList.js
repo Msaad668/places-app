dfbdf5da63daed6e3abaa92a055a2d946b8f33f5
unknown <mostafasaad55@gmail.com>
2020-05-29 04:26:31 +0200

    frontend main functionalities

--- frontend/src/places/components/PlaceList.js
+++ frontend/src/places/components/PlaceList.js
@@ -1,37 +1,36 @@
-import React from "react";
+import React from 'react';
 
-import "./PlaceList.css";
+import Card from '../../shared/components/UIElements/Card';
+import PlaceItem from './PlaceItem';
+import Button from '../../shared/components/FormElements/Button';
+import './PlaceList.css';
 
-import Card from "../../shared/components/UIElements/Card";
-import PlaceItem from "./PlaceItem";
-const PlaceList = (props) => {
+const PlaceList = props => {
   if (props.items.length === 0) {
     return (
-      <div className='place-list center'>
+      <div className="place-list center">
         <Card>
-          <h2>no places found, maybe create one?</h2>
-          <button>share place</button>
+          <h2>No places found. Maybe create one?</h2>
+          <Button to="/places/new">Share Place</Button>
         </Card>
       </div>
     );
   }
 
   return (
-    <ul className='place-list'>
-      {props.items.map((place) => {
-        return (
-          <PlaceItem
-            key={place.id}
-            id={place.id}
-            image={place.imageUrl}
-            title={place.title}
-            description={place.description}
-            address={place.address}
-            creatorId={place.creator}
-            coordinates={place.location}
-          />
-        );
-      })}
+    <ul className="place-list">
+      {props.items.map(place => (
+        <PlaceItem
+          key={place.id}
+          id={place.id}
+          image={place.imageUrl}
+          title={place.title}
+          description={place.description}
+          address={place.address}
+          creatorId={place.creator}
+          coordinates={place.location}
+        />
+      ))}
     </ul>
   );
 };
